86d93b20e85c81ea8af157becc24d0dc6905df66
CoolRuslanCool <cool3331982@mail.ru>
2016-10-17 23:25:00 +0300

    33

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,20 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello");
         bbb ff = new bbb();
         // write your code here
+        ArrayList<Integer> list = ff.makeArrayList();
+        for (int a=0;a<10;a++) {
+            list.add(a,((int)(Math.random()*20)));
+        }
+        for (Integer f:list) {
+            System.out.println(f);
+        }
+
     }
 }
--- src/com/company/bbb.java
+++ src/com/company/bbb.java
@@ -1,10 +1,16 @@
 package com.company;
 
+import java.util.ArrayList;
+
 /**
  * Created by ruslan on 17.10.16.
  */
 public class bbb {
     bbb () {
         System.out.println("I am hia");
+    }
+    ArrayList<Integer> makeArrayList() {
+        ArrayList<Integer> list = new ArrayList<>();
+        return list;
     }
 }
